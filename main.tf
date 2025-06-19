e646717e135317affe1399d3bbdb6ec2aef1d44d
Gabriel Pavanelli <pavanelli@outlook.com>
2025-06-19 19:04:20 -0300

    8 commit

--- main.tf
+++ main.tf
@@ -1,28 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
 }
 
-resource "google_container_cluster" "primary" {
-  name                     = var.cluster_name
-  location                 = var.region
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  ip_allocation_policy {}
+variable "project_id" {}
+variable "region" {
+  default = "us-central1"
+}
+variable "cluster_name" {
+  default = "gke-autopilot-cluster"
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  name     = "node-pool"
-  location = var.region
-  cluster  = google_container_cluster.primary.name
+resource "google_container_cluster" "primary" {
+  name             = var.cluster_name
+  location         = var.region
+  enable_autopilot = true
+}
 
-  node_config {
-    machine_type = "e2-medium"
-    disk_size_gb = 10
-    disk_type    = "pd-standard"
-    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-
-  initial_node_count = 1
+output "cluster_name" {
+  value = google_container_cluster.primary.name
 }
