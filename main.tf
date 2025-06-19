5887fb668d78cf43b76df4198b5dbc7c32b9f63b
Gabriel Pavanelli <pavanelli@outlook.com>
2025-06-19 19:15:37 -0300

    56578 commit

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
+provider "google" {
+  project = var.project_id
+  region  = var.region
 }
 
 resource "google_container_cluster" "primary" {
   name             = var.cluster_name
   location         = var.region
   enable_autopilot = true
-}
-
-output "cluster_name" {
-  value = google_container_cluster.primary.name
 }
