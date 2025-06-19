afa62ae3817fdaef828de15762f39fc9927e2acd
Gabriel Pavanelli <pavanelli@outlook.com>
2025-06-19 18:58:36 -0300

    5 commit

--- main.tf
+++ main.tf
@@ -4,9 +4,8 @@ provider "google" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
-
+  name                     = var.cluster_name
+  location                 = var.region
   remove_default_node_pool = true
   initial_node_count       = 1
 
@@ -15,16 +14,14 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_nodes" {
   name       = "node-pool"
-  location   = var.region
   cluster    = google_container_cluster.primary.name
+  location   = var.region
 
   node_config {
     machine_type = "e2-medium"
-    disk_size_gb = 50        # ou até 20 para teste
-    disk_type    = "pd-standard"  # ou "pd-balanced", evita usar "pd-ssd"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
+    disk_size_gb = 10
+    disk_type    = "pd-standard"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
   initial_node_count = 1
